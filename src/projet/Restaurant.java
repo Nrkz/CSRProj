6b2e57194d601ef8baa61c95837e13785cc475dc
Salerno Damien <damien.salerno35@gmail.com>
2019-12-04 13:43:36 +0100

    Restau

--- src/projet/Restaurant.java
+++ src/projet/Restaurant.java
@@ -2,14 +2,38 @@ package projet;
 
 public class Restaurant {
 	
-	static final int CLIENTS_MAX=25;
+	static final int CLIENTS_MAX = 25;
+	private static int nbClients;
+	private Client[] clients;
+	private Buffet buffet;
+	private Stand stand;
+	private Cuisinier cuisto;
 	
-	public static void main(String[] args) {
-		Buffet buffet = new Buffet();
-		Employe employe = new Employe(buffet);
-		for (int i=0;i<25;i++) {
-			Client client = new Client(buffet);
-		}
+	public Restaurant () {
+		this.buffet = new Buffet();
+		this.stand = new Stand();
+		this.cuisto = new Cuisinier();
+		this.nbClients = 0;
+		this.clients = new Client[50];
 		
+		for (int i = 0; i < clients.length; i ++) {
+			clients[i] = new Client();
+		}
 	}
+	
+	public static boolean addClients() {
+		if(nbClients < CLIENTS_MAX) {
+			nbClients++;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	
+	public static void removeClient() {
+		nbClients --;
+	}
+
 }
